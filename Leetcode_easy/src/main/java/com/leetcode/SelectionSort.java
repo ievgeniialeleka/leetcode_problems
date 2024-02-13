a26f8ba2f1bd1443b5df70d0bfb54b8bc2eb3818
Eve Leleka <ievgeniia8705@gmail.com>
2024-02-13 16:50:10 -0500

    Added Selection Sort class and unit tests for it

---
+++ Leetcode_easy/src/main/java/com/leetcode/SelectionSort.java
@@ -0,0 +1,19 @@
+package com.leetcode;
+
+public class SelectionSort {
+
+    public static int[] selectionSort(int[] a) {
+        for(int i = 0; i < a.length; i++) {
+            int min = i;
+            for(int j = i + 1; j < a.length; j++) {
+                if(a[j] < a[min]) {
+                   min = j;
+                }
+            }
+            int temp = a[i];
+            a[i] = a[min];
+            a[min] = temp;
+        }
+        return a;
+    }
+}
